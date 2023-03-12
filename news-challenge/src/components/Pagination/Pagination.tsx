a2bf1acb95d88b264e6ee45774611b93da861d76
rmnano <getserved06@gmail.com>
2023-03-12 13:15:39 +1100

    refine pagination
    refine pagination

--- news-challenge/src/components/Pagination/Pagination.tsx
+++ news-challenge/src/components/Pagination/Pagination.tsx
@@ -1,26 +1,38 @@
-import React, { FC } from 'react'
+import { FC, useMemo } from 'react'
 import $ from "./Pagination.module.css"
 import { Link, useLocation } from 'react-router-dom';
-import e from 'express';
 
 interface PaginationProps {
     totalNumber: number,
     numberPerPage?: number,
+    shownPageNum?: number,
     currentPage: number,
 }
 
 const Pagination: FC<PaginationProps> = ({ 
     totalNumber,
     numberPerPage = 10,
+    shownPageNum = 3,
     currentPage = 1,
  }) => {
     const {pathname} = useLocation()
 
-    console.log('numberPerPage', numberPerPage)
-
     const totalPages = Math.ceil(totalNumber / numberPerPage)
 
-    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    // return shown pagers
+    const pages = useMemo(() => {
+        // genereate whole pagers for all pages
+        const totalNumPage = Array.from({ length: Math.max(totalPages, shownPageNum)}, (_, i) => i+1);
+        // get offset for shown pagers by currentPage - (shownPage / 2)
+        const offset = Math.min(Math.max(currentPage - 1 - Math.floor(shownPageNum / 2), 0), totalPages - shownPageNum)
+        // get the window for shownPages by offset
+        const rs = totalNumPage.slice(offset, shownPageNum + offset);
+        // if currentPage is larger than showPageNum, put a default pager 1
+        if (currentPage >= shownPageNum) {
+            rs.unshift(1)
+        }
+        return rs
+    }, [totalPages, shownPageNum, currentPage])
 
     return (
         <div className={$.pagination}>
@@ -33,13 +45,13 @@ const Pagination: FC<PaginationProps> = ({
                     <Link
                         key={`link_${pathname}_${page}`}
                         to={`${page}`}
-                        className={`${$.paginationItem} ${currentPage === (page+1)?$.paginationNone: ''}`}
-                    >{page}</Link>
+                        className={`${$.paginationItem} ${currentPage === page?$.paginationNone: ''}`}
+                    >{page}{page === 1 && (currentPage - shownPageNum) >= 0? '...':''}</Link>
                 )
             })}
             <Link
                 to={`${currentPage+1}`}
-                className={`${$.paginationItem} ${currentPage >= totalPages - 1? $.paginationNone: ''}`}
+                className={`${$.paginationItem} ${currentPage > totalPages - 1? $.paginationNone: ''}`}
             >Next</Link>
         </div>
     )
